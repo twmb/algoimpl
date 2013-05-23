6fccd94d0794d9f7182a88c314394e87b15ecc49
twmb <travis.bischel@gmail.com>
2013-05-22 21:47:58 -0500

    modified to range over tests - more idiomatic

--- go/insertsort_test.go
+++ go/insertsort_test.go
@@ -4,26 +4,24 @@ import "testing"
 
 func TestSort(t *testing.T) {
 	// 100% line coverage
-	scrambled := []int{21, -10, 54, 0, 1098309}
-	sorted := []int{-10, 0, 21, 54, 1098309}
-	Sort(scrambled)
-	testSort(t, scrambled, sorted)
-
-	scrambled = []int{}
-	sorted = []int{}
-	Sort(scrambled)
-	testSort(t, scrambled, sorted)
-}
-
-func testSort(t *testing.T, scrambled, sorted []int) {
-	failed := false
-	for i, v := range scrambled {
-		if v != sorted[i] {
-			t.Errorf("Sort, position %v, sortval %v, supposed to be %v", i, v, sorted[i])
-			failed = true
-		}
+	tests := []struct {
+		In, Want []int
+	}{
+		{[]int{21, -10, 54, 0, 1098309}, []int{-10, 0, 21, 54, 1098309}},
+		{[]int{}, []int{}},
 	}
-	if failed {
-		t.Errorf("Failing slices: %v != %v", scrambled, sorted)
+
+	for _, test := range tests {
+		Sort(test.In)
+		failed := false
+		for i, v := range test.In {
+			if v != test.Want[i] {
+				t.Errorf("Sort, position %v, sortval %v, supposed to be %v", i, v, test.Want[i])
+				failed = true
+			}
+		}
+		if failed {
+			t.Errorf("Failing slices: %v != %v", test.In, test.Want)
+		}
 	}
 }
