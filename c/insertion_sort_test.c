1fde46534d22b6ab9a6a645e1e3de7f9e5f459c2
twmb <travis.bischel@gmail.com>
2013-05-25 18:28:02 -0500

    created insertion sort in c

---
+++ c/insertion_sort_test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "sort.h"
+
+int main(int argc, char **argv) {
+  int us1[1] = {-1};
+  int us2[5] = {4,3,2,1,2};
+  insertion_sort(us1, 0, 0);
+  insertion_sort(us2, 0, 5);
+  int prev = us2[0];
+  for (int i = 0; i < 5; i++) {
+    if (us2[i] < prev) {
+      printf("us2 error: index %d, val %d > index %d, val %d\n", i, us2[i], i-1, prev);
+      return -1;
+    }
+  }
+  printf("all good\n");
+}
+
---
+++ c/sort.h
@@ -0,0 +1,7 @@
+#ifndef TWMB_SORT
+#define TWMB_SORT
+
+// pointer to array to sort, start index, length
+void insertion_sort(int *arr, int start, int length);
+
+#endif
