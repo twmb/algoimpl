b8604220b42cfac9211ef6be0c7bf928e5ce824a
twmb <travis.bischel@gmail.com>
2013-05-28 15:59:32 -0500

    made iterative max subarray function

--- go/various/maxsubarray.go
+++ go/various/maxsubarray.go
@@ -23,15 +23,15 @@ func maxCrossingSubarray(array []int, from, to int) (li, ri, sum int) {
 	return li, ri + 1, lsum + rsum // one after last valid index
 }
 
-func MaxSubarray(array []int, from, to int) (li, ri, sum int) {
+func MaxSubarrayRecursive(array []int, from, to int) (li, ri, sum int) {
 	if from >= to-1 {
 		if to-from == 0 {
 			return from, to, 0
 		}
 		return from, to, array[from]
 	} else {
-		lli, lri, lv := MaxSubarray(array, from, (from+to)/2)
-		rli, rri, rv := MaxSubarray(array, (from+to)/2, to)
+		lli, lri, lv := MaxSubarrayRecursive(array, from, (from+to)/2)
+		rli, rri, rv := MaxSubarrayRecursive(array, (from+to)/2, to)
 		cli, cri, cv := maxCrossingSubarray(array, from, to)
 		if lv > rv && lv > cv {
 			return lli, lri, lv // left's left index, right index, sum
@@ -41,4 +41,30 @@ func MaxSubarray(array []int, from, to int) (li, ri, sum int) {
 			return cli, cri, cv // crossing left index, right index, sum
 		}
 	}
+}
+
+// iterative
+func MaxSubarray(array []int, from, to int) (li, ri, sum int) {
+  if to-from <= 1 {
+    if to-from == 1 {
+      return from, to, array[from]
+    }
+    return from, to, 0
+  }
+  hli, hri, hsum := from, from, array[from] // subarray right now
+  bli, bri, bsum := from, from, array[from] // best so far
+
+  for here := from+1; here < to ; here++ {
+    if hsum + array[here] > array[here] { // if subarray now is still 
+                                          // higher than when started
+      hri = here // slide right index right one more
+      hsum += array[here] // record new sum
+    } else { // else subarray lost, begin tracking from local valley
+      hli, hri, hsum  = here, here, array[here]
+    }
+    if hsum > bsum { // record highest sum while it is net positive
+      bli, bri, bsum = hli, hri, hsum
+    }
+  }
+  return bli, bri+1, bsum // here's right index is absolute, want one past
 }
