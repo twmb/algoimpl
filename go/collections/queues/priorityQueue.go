318afded7a472ead677c3baeb5706acd354be5bd
twmb <travis.bischel@gmail.com>
2013-06-14 21:54:24 -0500

    created priority queue and tests and modifying functions

---
+++ go/collections/queues/priorityQueue.go
@@ -0,0 +1,116 @@
+package queues
+
+import (
+	"errors"
+	"fmt"
+	"github.com/twmb/algoimpl/go/sort"
+)
+
+func left(i int) int   { return 2*i + 1 }     // 2 * (i + 1) - 1 because 0 indexed instead of 1
+func right(i int) int  { return 2*i + 2 }     // 2 * (i + 1) - 1 + 1
+func parent(i int) int { return (i+1)/2 - 1 } // (i + 1) / 2 - 1
+
+// This interface embeds the Sortable interface, meaning elements
+// can be compared and swapped. It extends the Sortable interface by
+// allowing access to individual elements or by appending to the collection.
+type ModSortable interface {
+	sort.Sortable
+	At(int) interface{}
+	Set(i int, val interface{}) error
+	Append(val interface{}) (ModSortable, error)
+}
+
+// A heap that can be appended to or can have individual values accessed
+// or changed.
+type ModifiableHeap struct {
+	collection ModSortable
+	size       int
+}
+
+func heapify(heap ModifiableHeap, i int) {
+	l := left(i)
+	r := right(i)
+	largestI := i
+	if l < heap.size && heap.collection.Less(i, l) {
+		largestI = l
+	}
+	if r < heap.size && heap.collection.Less(largestI, r) {
+		largestI = r
+	}
+	if largestI != i {
+		heap.collection.Swap(largestI, i)
+		heapify(heap, largestI)
+	}
+}
+
+// Returns a new priority queue. It can be modified using the heap
+// functions below, all of which run in O(lg n) time.
+func NewPriorityQueue(stuff ModSortable) *ModifiableHeap {
+	sort.BuildHeap(stuff)
+	return &ModifiableHeap{stuff, stuff.Len()}
+}
+
+// Returns the maximum of the heap.
+func (h *ModifiableHeap) Maximum() (interface{}, error) {
+	if h.collection.Len() > 0 {
+		return h.collection.At(0), nil
+	}
+	return nil, errors.New("Cannot call maximum on empty heap")
+}
+
+// Returns the maximum of the heap, reorganizes and decrements the size
+// by one.
+func (h *ModifiableHeap) ExtractMax() (interface{}, error) {
+	if h.size < 0 {
+		return 0, errors.New("heap underflow")
+	}
+	max := h.collection.At(0)
+	h.collection.Swap(0, h.size-1)
+	h.size--
+	heapify(*h, 0)
+	return max, nil
+}
+
+// This function will change the value at the index and will reorganize
+// the priority queue as appropriate.
+func (h *ModifiableHeap) ChangeValue(i int, to interface{}) error {
+	if i >= h.collection.Len() {
+		return fmt.Errorf("Cannot change index %v, out of bounds of collection (length %v)", i, h.collection.Len())
+	}
+	err := h.collection.Set(i, to)
+	if err != nil {
+		return err
+	}
+	pi := parent(i)
+	// shuffle up if the changed value is now larger than the parents
+	// O(lg n), O(1) if it is not
+	for i > 0 && h.collection.Less(pi, i) {
+		h.collection.Swap(pi, i)
+		i = pi
+		pi = parent(i)
+	}
+	// shuffle down if changed value smaller than children
+	// O(lg n), O(1) if it is not
+	heapify(*h, i)
+	return nil
+}
+
+// The caller must make sure that the Append function returns the same
+// type that is originally used in NewPriorityQueue.
+// This function will insert a new value into a priority queue and will
+// error if the value is not of the same type. If the return value of
+// Append is not the same as what was originally used in NewPriorityQueue,
+// this function will panic.
+func (h *ModifiableHeap) Insert(val interface{}) error {
+	returned, err := h.collection.Append(val)
+	if err != nil {
+		return err
+	}
+	h.collection = returned
+	h.size++
+	err = h.ChangeValue(h.size-1, val) // yes, changes twice, cum at me bra
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ go/collections/queues/priorityQueue_test.go
@@ -0,0 +1,217 @@
+package queues
+
+import (
+	"errors"
+	"testing"
+)
+
+type Ints []int
+
+func (p Ints) Len() int             { return len(p) }
+func (p Ints) Less(i, j int) bool   { return p[i] < p[j] }
+func (p Ints) Swap(i, j int)        { p[i], p[j] = p[j], p[i] }
+func (p Ints) At(i int) interface{} { return p[i] }
+func (p Ints) Set(i int, val interface{}) error {
+	v, ok := val.(int)
+	if ok {
+		p[i] = v
+		return nil
+	}
+	return errors.New("Set() passed in type differs from what the collection can hold")
+}
+func (p Ints) Append(val interface{}) (ModSortable, error) {
+	v, ok := val.(int)
+	if ok {
+		p = append(p, v)
+		return &p, nil
+	}
+	return &p, errors.New("Append: passed in type is not int")
+}
+
+func TestNewPriorityQueue(t *testing.T) {
+	wantInts0 := Ints([]int{})
+	wantInts1 := Ints([]int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1})
+	tests := []struct {
+		In   Ints
+		Want *ModifiableHeap
+	}{
+		{ // test empty
+			Ints([]int{}),
+			&ModifiableHeap{collection: &wantInts0, size: 0},
+		},
+		{
+			Ints([]int{4, 1, 3, 2, 16, 9, 10, 14, 8, 7}),
+			&ModifiableHeap{collection: &wantInts1, size: len(wantInts1)},
+		},
+	}
+	for _, test := range tests {
+		got := NewPriorityQueue(test.In)
+		ints, ok := got.collection.(Ints)
+		if !ok {
+			t.Errorf("yo %v not ints, wtf yo", ints)
+			continue
+		}
+		failed := false
+		for i, v := range ints {
+			if v != test.Want.collection.At(i) {
+				failed = true
+				break
+			}
+		}
+		if failed {
+			t.Errorf("Failing Ints: result %v != want %v", test.In, test.Want)
+		}
+	}
+}
+
+// called with a heap
+// if not, dumb, but result same
+func TestMaximum(t *testing.T) {
+	inputInts0 := Ints([]int{})
+	inputInts1 := Ints([]int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1})
+	tests := []struct {
+		Caller    *ModifiableHeap
+		Want      int
+		WantError string
+	}{
+		{
+			&ModifiableHeap{collection: &inputInts0, size: len(inputInts0)},
+			0,
+			"Cannot call maximum on empty heap",
+		},
+		{
+			&ModifiableHeap{collection: &inputInts1, size: len(inputInts1)},
+			16,
+			"",
+		},
+	}
+	for _, test := range tests {
+		got, gotErr := test.Caller.Maximum()
+		if gotErr != nil {
+			if gotErr.Error() != test.WantError {
+				t.Errorf("Incorrect error received")
+			}
+			continue
+		}
+		gotInt, ok := got.(int)
+		if !ok {
+			t.Errorf("Could not convert returned value %v to int", gotInt)
+			continue
+		}
+		if gotInt != test.Want {
+			t.Errorf("Incorrect maximum, received %v, wanted %v", gotInt, test.Want)
+		}
+	}
+}
+
+func TestChangeValue(t *testing.T) {
+	inputInts0 := Ints([]int{})
+	wantInts0 := Ints([]int{})
+	inputInts1 := Ints([]int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1})
+	wantInts1 := Ints([]int{16, 15, 10, 14, 7, 9, 3, 2, 8, 1})
+	inputInts2 := Ints([]int{1})
+	wantInts2 := Ints([]int{1})
+	tests := []struct {
+		Caller       *ModifiableHeap
+		InIToChange  int
+		InIValChange interface{}
+		WantInts     Ints
+		WantError    string
+	}{
+		{
+			&ModifiableHeap{collection: &inputInts0, size: len(inputInts0)},
+			0,
+			0,
+			wantInts0,
+			"Cannot change index 0, out of bounds of collection (length 0)",
+		},
+		{
+			&ModifiableHeap{collection: &inputInts1, size: len(inputInts1)},
+			8,
+			15,
+			wantInts1,
+			"",
+		},
+		{
+			&ModifiableHeap{collection: &inputInts2, size: len(inputInts2)},
+			0,
+			"hello",
+			wantInts2,
+			"Set() passed in type differs from what the collection can hold",
+		},
+	}
+	for _, test := range tests {
+		gotErr := test.Caller.ChangeValue(test.InIToChange, test.InIValChange)
+		if gotErr != nil {
+			if gotErr.Error() != test.WantError {
+				t.Errorf("Incorrect error: received %v, wanted %v", gotErr, test.WantError)
+			}
+			continue
+		}
+		changedInts := test.Caller.collection.(*Ints)
+		failed := false
+		for i, v := range *changedInts {
+			if v != test.WantInts[i] {
+				failed = true
+				break
+			}
+		}
+		if failed {
+			t.Errorf("Failing Ints: result %v != want %v", changedInts, test.WantInts)
+		}
+	}
+}
+
+func TestInsert(t *testing.T) {
+	inputInts0 := Ints([]int{})
+	wantInts0 := Ints([]int{0})
+	inputInts1 := Ints([]int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1})
+	wantInts1 := Ints([]int{16, 15, 10, 8, 14, 9, 3, 2, 4, 1, 7})
+	inputInts2 := Ints([]int{1})
+	wantInts2 := Ints([]int{1})
+	tests := []struct {
+		Caller      *ModifiableHeap
+		InValChange interface{}
+		WantInts    Ints
+		WantError   string
+	}{
+		{
+			&ModifiableHeap{collection: &inputInts0, size: len(inputInts0)},
+			0,
+			wantInts0,
+			"",
+		},
+		{
+			&ModifiableHeap{collection: &inputInts1, size: len(inputInts1)},
+			15,
+			wantInts1,
+			"",
+		},
+		{
+			&ModifiableHeap{collection: &inputInts2, size: len(inputInts2)},
+			"hello",
+			wantInts2,
+			"Append: passed in type is not int",
+		},
+	}
+	for _, test := range tests {
+		gotErr := test.Caller.Insert(test.InValChange)
+		if gotErr != nil {
+			if gotErr.Error() != test.WantError {
+				t.Errorf("Incorrect error: received %v, wanted %v", gotErr, test.WantError)
+			}
+			continue
+		}
+		changedInts := test.Caller.collection.(*Ints)
+		failed := false
+		for i, v := range *changedInts {
+			if v != test.WantInts[i] {
+				failed = true
+				break
+			}
+		}
+		if failed {
+			t.Errorf("Failing Ints: result %v != want %v", changedInts, test.WantInts)
+		}
+	}
+}
