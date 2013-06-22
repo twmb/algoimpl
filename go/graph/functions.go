a7cbf22ec45cf4bb4b686676bdfc11d8a583eb1f
twmb <travis.bischel@gmail.com>
2013-06-21 20:42:06 -0500

    created strongly connected components function

--- go/graph/functions.go
+++ go/graph/functions.go
@@ -2,11 +2,12 @@ package graph
 
 const (
 	unseen = iota
-	visited
+	seen
 )
 
+// O(V + E)
 func dfs(node *Node, finishList *[]*Node) {
-	node.state = visited
+	node.state = seen
 	for _, adjacentNode := range node.adjacent {
 		if adjacentNode.state == unseen {
 			adjacentNode.parent = node
@@ -18,8 +19,9 @@ func dfs(node *Node, finishList *[]*Node) {
 
 // Topologically sorts a directed, acyclic graph.
 // If the graph is cyclic, the sort order will change
-// based on which node the sort starts on.
+// based on which node the sort starts on. O(V+E) complexity.
 func TopologicalSort(g *Graph) []*Node {
+	g.lazyInit()
 	sorted := make([]*Node, 0)
 	// sort preorder (first jacket, then shirt)
 	for _, node := range g.nodes {
@@ -27,10 +29,59 @@ func TopologicalSort(g *Graph) []*Node {
 			dfs(node, &sorted)
 		}
 	}
-	// now make post order for correct sort (jacket follows shirt)
+	// now make post order for correct sort (jacket follows shirt). O(V)
 	length := len(sorted)
 	for i := 0; i < length/2; i++ {
 		sorted[i], sorted[length-i-1] = sorted[length-i-1], sorted[i]
 	}
 	return sorted
+}
+
+// Returns reversed copy of the directed graph g. O(V+E) complexity.
+func ReverseGraph(g *Graph) *Graph {
+	reversed, _ := New("directed")
+	// O(V)
+	for _, node := range g.nodes {
+		reversed.MakeNode(node.Value)
+	}
+	// O(V + E)
+	for _, node := range g.nodes {
+		for _, adjacent := range node.adjacent {
+			reversed.Connect(reversed.nodes[adjacent.graphIndex], reversed.nodes[node.graphIndex])
+		}
+	}
+	return reversed
+}
+
+// Returns a slice of strongly connected nodes on a directed graph.
+// If passed an undirected graph, returns nil.
+// The returned components have reversed, nonexclusive edges.
+// For example, if this is passed the graph
+//     a->b, c
+//     b->a, c
+//     c
+// will return components
+//     [[c->a, b], [b->a], [a->b]]
+// where -> represents the edges that the node contains.
+// O(V+E) complexity.
+func StronglyConnectedComponents(g *Graph) [][]*Node {
+	if g.kind == 0 {
+		return nil
+	}
+	g.lazyInit()
+	components := make([][]*Node, 0)
+	finishOrder := TopologicalSort(g)
+	for i := range finishOrder {
+		finishOrder[i].state = unseen
+	}
+	// creates a reversed graph with empty parents
+	reversed := ReverseGraph(g)
+	for _, sink := range finishOrder {
+		if reversed.nodes[sink.graphIndex].state == unseen {
+			component := make([]*Node, 0)
+			dfs(reversed.nodes[sink.graphIndex], &component)
+			components = append(components, component)
+		}
+	}
+	return components
 }
--- go/graph/functions_test.go
+++ go/graph/functions_test.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -41,9 +42,64 @@ func TestTopologicalSort(t *testing.T) {
 	wantOrder[7] = nodes[1] // tie
 	wantOrder[8] = nodes[2] // jacket
 	result := TopologicalSort(graph)
+	fmt.Println(result)
 	for i := range result {
 		if result[i] != wantOrder[i] {
 			t.Errorf("index %v in result != wanted, value: %v, want value: %v", i, result[i].Value, wantOrder[i].Value)
+		}
+	}
+}
+
+func TestStronglyConnectedComponents(t *testing.T) {
+	graph, err := New("directed")
+	if err != nil {
+		t.Errorf("TestStronglyConnectedComponents: unable to create directed graph")
+	}
+	nodes := graph.Nodes()
+	// create SCC graph on page 616 of CLRS ed 3
+	nodes = append(nodes, graph.MakeNode("c")) //0
+	nodes = append(nodes, graph.MakeNode("g")) //1
+	nodes = append(nodes, graph.MakeNode("f")) //2
+	nodes = append(nodes, graph.MakeNode("h")) //3
+	nodes = append(nodes, graph.MakeNode("d")) //4
+	nodes = append(nodes, graph.MakeNode("b")) //5
+	nodes = append(nodes, graph.MakeNode("e")) //6
+	nodes = append(nodes, graph.MakeNode("a")) //7
+	graph.Connect(nodes[0], nodes[1])
+	graph.Connect(nodes[0], nodes[4])
+	graph.Connect(nodes[1], nodes[2])
+	graph.Connect(nodes[1], nodes[3])
+	graph.Connect(nodes[2], nodes[1])
+	graph.Connect(nodes[3], nodes[3])
+	graph.Connect(nodes[4], nodes[3])
+	graph.Connect(nodes[4], nodes[0])
+	graph.Connect(nodes[5], nodes[6])
+	graph.Connect(nodes[5], nodes[0])
+	graph.Connect(nodes[5], nodes[2])
+	graph.Connect(nodes[6], nodes[2])
+	graph.Connect(nodes[6], nodes[7])
+	graph.Connect(nodes[7], nodes[5])
+	graph.verify(t)
+	want := make([][]*Node, 4)
+	want[0] = make([]*Node, 3)
+	want[1] = make([]*Node, 2)
+	want[2] = make([]*Node, 2)
+	want[3] = make([]*Node, 1)
+	want[0][0] = nodes[6]
+	want[0][1] = nodes[7]
+	want[0][2] = nodes[5]
+	want[1][0] = nodes[0]
+	want[1][1] = nodes[4]
+	want[2][0] = nodes[2]
+	want[2][1] = nodes[1]
+	want[3][0] = nodes[3]
+	components := StronglyConnectedComponents(graph)
+	fmt.Println(components)
+	for j := range components {
+		for i := range want[j] {
+			if !valueSliceContains(components[j], want[j][i].Value) {
+				t.Errorf("component slice does %v does not contained wanted value %v", components[j], want[j][i].Value)
+			}
 		}
 	}
 }
