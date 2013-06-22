78a2225a2caa448bd16d5158a7048f1d2dc9d91a
twmb <travis.bischel@gmail.com>
2013-06-21 22:03:01 -0500

    un-export functions - only HeapSort exposed now

--- go/sort/heap.go
+++ go/sort/heap.go
@@ -2,7 +2,7 @@ package sort
 
 // Shuffles a smaller value at index i in a heap
 // down to the appropriate spot. Complexity is O(lg n).
-func ShuffleDown(heap Sortable, i, end int) {
+func shuffleDown(heap Sortable, i, end int) {
 	for {
 		l := 2*i + 1
 		if l >= end { // int overflow? (in go source)
@@ -20,33 +20,20 @@ func ShuffleDown(heap Sortable, i, end int) {
 	}
 }
 
-// Shuffles a larger value in a heap at index i
-// up to the appropriate spot. Complexity is O(lg n).
-func ShuffleUp(heap Sortable, i int) {
-	for {
-		pi := (i - 1) / 2 // (i + 1) / 2 - 1, parent
-		if i == pi || !heap.Less(pi, i) {
-			break
-		}
-		heap.Swap(pi, i)
-		i = pi
-	}
-}
-
 // Creates a max heap out of an unorganized Sortable collection.
 // Runs in O(n) time.
-func BuildHeap(stuff Sortable) {
+func buildHeap(stuff Sortable) {
 	for i := stuff.Len()/2 - 1; i >= 0; i-- { // start at first non leaf (equiv. to parent of last leaf)
-		ShuffleDown(stuff, i, stuff.Len())
+		shuffleDown(stuff, i, stuff.Len())
 	}
 }
 
 // Runs HeapSort on a Sortable collection.
 // Runs in O(n * lg n) time, but amortizes worse than quicksort
 func HeapSort(stuff Sortable) {
-	BuildHeap(stuff)
+	buildHeap(stuff)
 	for i := stuff.Len() - 1; i > 0; i-- {
 		stuff.Swap(0, i) // put max at end
-		ShuffleDown(stuff, 0, i)
+		shuffleDown(stuff, 0, i)
 	}
 }
--- go/sort/heap_test.go
+++ go/sort/heap_test.go
@@ -3,32 +3,6 @@ package sort
 import "testing"
 
 // uses Ints from sort_test
-// the Heapify function is tested with the both of these tests
-
-func TestBuildHeap(t *testing.T) {
-	tests := []struct {
-		In, Want Ints
-	}{
-		{Ints([]int{}), Ints([]int{})},
-		{
-			Ints([]int{4, 1, 3, 2, 16, 9, 10, 14, 8, 7}),
-			Ints([]int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}),
-		},
-	}
-	for _, test := range tests {
-		BuildHeap(test.In)
-		failed := false
-		for i, v := range test.In {
-			if v != test.Want[i] {
-				failed = true
-				break
-			}
-		}
-		if failed {
-			t.Errorf("Failing Ints: result %v != want %v", test.In, test.Want)
-		}
-	}
-}
 
 func TestHeapSort(t *testing.T) {
 	tests := []struct {
