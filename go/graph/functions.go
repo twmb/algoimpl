b7fef4b11c106e7de4491bbd000b1867ff61a5c1
twmb <travis.bischel@gmail.com>
2013-06-23 04:30:33 -0500

    updated to use Node struct, added MinimumSpanningTree algorithm

--- go/graph/functions.go
+++ go/graph/functions.go
@@ -1,28 +1,36 @@
 package graph
 
+import (
+	"container/heap"
+)
+
 const (
 	unseen = iota
 	seen
+	queued = 1 << 30 // assume that no weight will be > 2^30
 )
 
-// O(V + E)
-func (g *Graph) dfs(node *Node, finishList *[]*Node) {
+// O(V + E). It does not matter to traverse back
+// on a bidirectional edge, because any vertex dfs is
+// recursing on is marked as visited and won't be visited
+// again anyway.
+func (g *Graph) dfs(node *node, finishList *[]Node) {
 	node.state = seen
-	for _, adjacentNode := range g.adjacents[node] {
-		if adjacentNode.state == unseen {
-			adjacentNode.parent = node
-			g.dfs(adjacentNode, finishList)
+	for _, edge := range g.edges[node] {
+		if edge.End.node.state == unseen {
+			edge.End.node.parent = node
+			g.dfs(edge.End.node, finishList)
 		}
 	}
-	*finishList = append(*finishList, node)
+	*finishList = append(*finishList, node.container)
 }
 
 // Topologically sorts a directed acyclic graph.
 // If the graph is cyclic, the sort order will change
 // based on which node the sort starts on. O(V+E) complexity.
-func TopologicalSort(g *Graph) []*Node {
+func TopologicalSort(g *Graph) []Node {
 	g.lazyInit()
-	sorted := make([]*Node, 0, len(g.nodes))
+	sorted := make([]Node, 0, len(g.nodes))
 	// sort preorder (first jacket, then shirt)
 	for _, node := range g.nodes {
 		if node.state == unseen {
@@ -38,16 +46,20 @@ func TopologicalSort(g *Graph) []*Node {
 }
 
 // Returns reversed copy of the directed graph g. O(V+E) complexity.
+// This function can be used to copy an undirected graph.
 func Reverse(g *Graph) *Graph {
 	reversed, _ := New("directed")
+	if g.kind == Undirected {
+		reversed, _ = New("undirected")
+	}
 	// O(V)
 	for _ = range g.nodes {
 		reversed.MakeNode()
 	}
 	// O(V + E)
 	for _, node := range g.nodes {
-		for _, adjacent := range g.adjacents[node] {
-			reversed.Connect(reversed.nodes[adjacent.Index], reversed.nodes[node.Index])
+		for _, edge := range g.edges[node] {
+			reversed.Connect(reversed.nodes[edge.End.node.graphIndex].container, reversed.nodes[node.graphIndex].container)
 		}
 	}
 	return reversed
@@ -64,24 +76,125 @@ func Reverse(g *Graph) *Graph {
 //     [[c->a, b], [b->a], [a->b]]
 // where -> represents the edges that the node contains.
 // O(V+E) complexity.
-func StronglyConnectedComponents(g *Graph) [][]*Node {
-	if g.kind == 0 {
+func StronglyConnectedComponents(g *Graph) [][]Node {
+	if g.kind == Undirected {
 		return nil
 	}
 	g.lazyInit()
-	components := make([][]*Node, 0)
+	components := make([][]Node, 0)
 	finishOrder := TopologicalSort(g)
 	for i := range finishOrder {
-		finishOrder[i].state = unseen
+		finishOrder[i].node.state = unseen
 	}
 	// creates a reversed graph with empty parents
 	reversed := Reverse(g)
 	for _, sink := range finishOrder {
-		if reversed.nodes[sink.Index].state == unseen {
-			component := make([]*Node, 0)
-			reversed.dfs(reversed.nodes[sink.Index], &component)
+		if reversed.nodes[sink.node.graphIndex].state == unseen {
+			component := make([]Node, 0)
+			reversed.dfs(reversed.nodes[sink.node.graphIndex], &component)
 			components = append(components, component)
 		}
 	}
 	return components
+}
+
+// This function will sort the edges vertices and return a the edges
+// in sorted order. An edge is represented by a length 2 array
+// containing the start node and the end node.
+//func Sort(g *Graph) []Edge { // O(V+E)
+//	var edges []Edge
+//	if g.kind == Directed {
+//		edges = make([]Edge, 0, len(g.edges))
+//	} else {
+//		edges = make([]Edge, 0, len(g.edges)/2)
+//	}
+//	for _, node := range g.nodes { // O(V)
+//		for _, edge := range g.edges[node] { // O(E)
+//			if g.kind == Directed || edge.Start.node != node {
+//				edges = append(edges, edge)
+//			}
+//		}
+//	}
+//	sort.Sort(edgeSlice(edges)) // O(E)
+//	return edges
+//}
+
+// This function will return the edges corresponding to the
+// minimum spanning tree in the graph based off of the edge's weight values.
+func MinimumSpanningTree(g *Graph) []Edge {
+	// create priority queue for vertices
+	nodesBase := nodeSlice(make([]*node, 0))
+	nodes := &nodesBase
+	heap.Init(nodes)
+	for _, node := range g.nodes[1:] {
+		node.state = 1<<30 - 1 // bit 31 is for queue testing
+		node.state |= queued
+		heap.Push(nodes, node)
+	}
+	g.nodes[0].state = 0
+	heap.Push(nodes, g.nodes[0]) // now add root element
+
+	for nodes.Len() > 0 {
+		min := heap.Pop(nodes).(*node)
+		for _, edge := range g.edges[min] {
+			v := edge.Start.node // get the other side of the edge
+			if min == edge.Start.node {
+				v = edge.End.node
+			}
+			if nodes.QueueContains(v) && *edge.Weight < (v.state & ^queued) {
+				v.parent = min
+				heap.Remove(nodes, v.data)      // remove it
+				v.state = *edge.Weight | queued // update state, add queue bit
+				heap.Push(nodes, v)             // add it back in, WORD, O(lg n) update key time!
+			}
+		}
+	}
+
+	mst := make([]Edge, 0)
+	for i := range g.nodes {
+		if g.nodes[i].parent != nil {
+			mst = append(mst, g.edgeBetween(g.nodes[i], g.nodes[i].parent))
+		}
+	}
+
+	return mst
+}
+
+// only called when the graph is guaranteed to have an edge
+// between the two nodes
+func (g *Graph) edgeBetween(v, u *node) Edge {
+	for _, edge := range g.edges[u] {
+		// one of the two is always u
+		if edge.Start.node == v || edge.End.node == v {
+			return edge
+		}
+	}
+	return Edge{}
+}
+
+type nodeSlice []*node
+
+func (n *nodeSlice) Push(x interface{}) {
+	p := x.(*node)
+	p.data = len(*n) // index into heap
+	*n = append(*n, x.(*node))
+}
+func (n *nodeSlice) Pop() interface{} {
+	rNode := (*n)[len(*n)-1]
+	rNode.state &= ^queued
+	*n = (*n)[0 : len(*n)-1]
+	return rNode
+}
+func (n nodeSlice) Len() int {
+	return len(n)
+}
+func (n nodeSlice) Less(i, j int) bool {
+	return n[i].state & ^queued < n[j].state & ^queued
+}
+func (n nodeSlice) Swap(i, j int) {
+	n[j].data, n[i].data = n[i].data, n[j].data // swap data containing indices
+	n[j], n[i] = n[i], n[j]
+}
+func (n nodeSlice) QueueContains(node *node) bool { // extend heap interface
+	return node.state&queued > 0
 }
