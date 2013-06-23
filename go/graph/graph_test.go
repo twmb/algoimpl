ce35da29a8f5d63bacd1da7b82ab6643e7385668
twmb <travis.bischel@gmail.com>
2013-06-23 04:29:51 -0500

    updated graph test as appropriate to take advantage of Node struct

--- go/graph/graph_test.go
+++ go/graph/graph_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 )
 
-func (g *Graph) edges(from, to *Node) bool {
-	for _, v := range g.adjacents[from] {
-		if v == to {
+func (g *Graph) edgeBack(from, to *node) bool {
+	for _, v := range g.edges[from] {
+		if v.End.node == to || v.Start.node == to && v.End.node == from {
 			return true
 		}
 	}
@@ -16,20 +16,20 @@ func (g *Graph) edges(from, to *Node) bool {
 func (g *Graph) verify(t *testing.T) {
 	// over all the nodes
 	for i, node := range g.nodes {
-		if node.Index != i {
-			t.Errorf("node's graph index %v != actual graph index %v", node.Index, i)
+		if node.graphIndex != i {
+			t.Errorf("node's graph index %v != actual graph index %v", node.graphIndex, i)
 		}
 		// over each adjacent node
-		for _, adjacentNode := range g.adjacents[node] {
+		for _, edge := range g.edges[node] {
 			// check that the graph contains it in the correct position
-			if adjacentNode.Index >= len(g.nodes) ||
-				g.nodes[adjacentNode.Index] != adjacentNode {
-				t.Errorf("adjacent node %v does not belong to the graph", adjacentNode)
+			if edge.End.node.graphIndex >= len(g.nodes) ||
+				g.nodes[edge.End.node.graphIndex] != edge.End.node {
+				t.Errorf("adjacent node %v does not belong to the graph", edge.End)
 			}
 			// if the graph is undirected, check that the adjacent node contains the original node back
-			if g.kind == 0 {
-				if !g.edges(adjacentNode, node) {
-					t.Errorf("undirected graph: node %v has adjacent node %v, adjacent node doesn't contain back", node, adjacentNode)
+			if g.kind == Undirected {
+				if !g.edgeBack(edge.End.node, node) {
+					t.Errorf("undirected graph: node %v has adjacent node %v, adjacent node doesn't contain back", node, edge.End)
 				}
 			}
 		}
@@ -69,30 +69,20 @@ func TestMakeNode(t *testing.T) {
 	if err != nil {
 		t.Errorf("TestMakeNode: unable to create undirected graph")
 	}
-	nodes := make(map[int]int, 0)
+	nodes := make(map[Node]int, 0)
 	for i := 0; i < 10; i++ {
-		nodes[graph.MakeNode().Index] = i
+		nodes[graph.MakeNode()] = i
 	}
 	graph.verify(t)
-	for i, node := range graph.nodes {
-		if nodes[node.Index] != i {
-			t.Errorf("Node at index %v != %v, wrong!", i, i)
-		}
-	}
 	graph, err = New("directed")
 	if err != nil {
 		t.Errorf("TestMakeNode: unable to create directed graph")
 	}
-	nodes = make(map[int]int, 0)
+	nodes = make(map[Node]int, 0)
 	for i := 0; i < 10; i++ {
-		nodes[graph.MakeNode().Index] = i
+		nodes[graph.MakeNode()] = i
 	}
 	graph.verify(t)
-	for i, node := range graph.nodes {
-		if nodes[node.Index] != i {
-			t.Errorf("Node at index %v != %v, wrong!", i, i)
-		}
-	}
 }
 
 func TestConnect(t *testing.T) {
@@ -100,19 +90,18 @@ func TestConnect(t *testing.T) {
 	if err != nil {
 		t.Errorf("TestMakeNode: unable to create undirected graph")
 	}
-	mapped := make(map[*Node]int, 0)
+	mapped := make(map[int]Node, 0)
 	for i := 0; i < 10; i++ {
-		mapped[graph.MakeNode()] = i
+		mapped[i] = graph.MakeNode()
 	}
-	nodes := graph.nodes
 	for j := 0; j < 5; j++ {
 		for i := 0; i < 10; i++ {
-			graph.Connect(nodes[i], nodes[(i+1+j)%len(nodes)])
+			graph.Connect(mapped[i], mapped[(i+1+j)%10])
 		}
 	}
 	graph.verify(t)
 	for i, node := range graph.nodes {
-		if mapped[node] != i {
+		if mapped[i].node != node {
 			t.Errorf("Node at index %v != %v, wrong!", i, i)
 		}
 	}
@@ -120,20 +109,19 @@ func TestConnect(t *testing.T) {
 	if err != nil {
 		t.Errorf("TestMakeNode: unable to create directed graph")
 	}
-	mapped = make(map[*Node]int, 0)
+	mapped = make(map[int]Node, 0)
 	for i := 0; i < 10; i++ {
-		mapped[graph.MakeNode()] = i
+		mapped[i] = graph.MakeNode()
 	}
-	nodes = graph.nodes
 	for j := 0; j < 5; j++ {
 		for i := 0; i < 10; i++ {
-			graph.Connect(nodes[i], nodes[(i+1+j)%len(nodes)])
+			graph.Connect(mapped[i], mapped[(i+1+j)%10])
 		}
 	}
 	graph.verify(t)
 	for i, node := range graph.nodes {
-		if mapped[node] != i {
-			t.Errorf("Node at index %v = %v, != %v, wrong!", i, mapped[node], i)
+		if mapped[i].node != node {
+			t.Errorf("Node at index %v = %v, != %v, wrong!", i, mapped[i], node)
 		}
 	}
 }
