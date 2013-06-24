5b3987802911cfed5eb49e975e0375400d0037f7
twmb <travis.bischel@gmail.com>
2013-06-24 16:10:20 -0500

    change connect to connect weight to make simpler

--- go/graph/example_mst_test.go
+++ go/graph/example_mst_test.go
@@ -17,35 +17,20 @@ func ExampleGraph_MinimumSpanningTree() {
 	nodes['g'] = g.MakeNode()
 	nodes['h'] = g.MakeNode()
 	nodes['i'] = g.MakeNode()
-	edges := make([]graph.Edge, 0)
-	edges = append(edges, *g.Connect(nodes['a'], nodes['b']))
-	edges = append(edges, *g.Connect(nodes['a'], nodes['h']))
-	edges = append(edges, *g.Connect(nodes['b'], nodes['c']))
-	edges = append(edges, *g.Connect(nodes['b'], nodes['h']))
-	edges = append(edges, *g.Connect(nodes['c'], nodes['d']))
-	edges = append(edges, *g.Connect(nodes['c'], nodes['f']))
-	edges = append(edges, *g.Connect(nodes['c'], nodes['i']))
-	edges = append(edges, *g.Connect(nodes['d'], nodes['e']))
-	edges = append(edges, *g.Connect(nodes['d'], nodes['f']))
-	edges = append(edges, *g.Connect(nodes['e'], nodes['f']))
-	edges = append(edges, *g.Connect(nodes['f'], nodes['g']))
-	edges = append(edges, *g.Connect(nodes['g'], nodes['h']))
-	edges = append(edges, *g.Connect(nodes['g'], nodes['i']))
-	edges = append(edges, *g.Connect(nodes['h'], nodes['i']))
-	*edges[0].Weight = 4
-	*edges[1].Weight = 8
-	*edges[2].Weight = 8
-	*edges[3].Weight = 11
-	*edges[4].Weight = 7
-	*edges[5].Weight = 4
-	*edges[6].Weight = 2
-	*edges[7].Weight = 9
-	*edges[8].Weight = 14
-	*edges[9].Weight = 10
-	*edges[10].Weight = 2
-	*edges[11].Weight = 1
-	*edges[12].Weight = 6
-	*edges[13].Weight = 7
+	g.ConnectWeight(nodes['a'], nodes['b'], 4)
+	g.ConnectWeight(nodes['a'], nodes['h'], 8)
+	g.ConnectWeight(nodes['b'], nodes['c'], 8)
+	g.ConnectWeight(nodes['b'], nodes['h'], 11)
+	g.ConnectWeight(nodes['c'], nodes['d'], 7)
+	g.ConnectWeight(nodes['c'], nodes['f'], 4)
+	g.ConnectWeight(nodes['c'], nodes['i'], 2)
+	g.ConnectWeight(nodes['d'], nodes['e'], 9)
+	g.ConnectWeight(nodes['d'], nodes['f'], 14)
+	g.ConnectWeight(nodes['e'], nodes['f'], 10)
+	g.ConnectWeight(nodes['f'], nodes['g'], 2)
+	g.ConnectWeight(nodes['g'], nodes['h'], 1)
+	g.ConnectWeight(nodes['g'], nodes['i'], 6)
+	g.ConnectWeight(nodes['h'], nodes['i'], 7)
 	mst := g.MinimumSpanningTree()
 	weightSum := 0
 	for i := range mst {
