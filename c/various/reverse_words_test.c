fe9456536bffac222d318595564a1634f14bed9f
twmb <travis.bischel@gmail.com>
2013-06-28 14:10:31 -0500

    fixed test, char * = read only, [] = stack

--- c/various/reverse_words_test.c
+++ c/various/reverse_words_test.c
@@ -6,12 +6,23 @@
 int main(int argc, char **argv) {
 //  char line[80];
 //  fgets(line, sizeof(line), stdin);
-  char *line = "hello this is easy\n";
+  char line[] = "hello this is easy\n";
   printf("%s\n", line);
   reverse_words(line);
   printf("%s\n", line);
   if (strcmp(line, "easy is this hello\n")) {
     printf("error, line %s not equal to 'easy is this hello\n'", line);
+  }
+  line[0] = '\0';
+  reverse_words(line);
+  if (strcmp(line, "")) {
+    printf("error, line %s not equal to ''", line);
+  }
+  line[0] = '\n';
+  line[1] = '\0';
+  reverse_words(line);
+  if (strcmp(line, "\n")) {
+    printf("error, line %s not equal to '\n'", line);
   }
 }
 
