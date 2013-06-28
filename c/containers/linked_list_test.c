28413897b01deed9c5ca9a1abe4ebb92aee2b635
twmb <travis.bischel@gmail.com>
2013-06-28 17:33:59 -0500

    return failure or not failure

--- c/containers/linked_list_test.c
+++ c/containers/linked_list_test.c
@@ -5,20 +5,15 @@
 
 int main() {
   linked_list mine = new_linked_list();
+  int failed = 0;
   push_ll(&mine, (void *)3);
   push_ll(&mine, (void *)2);
   push_ll(&mine, (void *)2);
-  item *head = mine.head;
-  while (head != NULL) {
-    printf("hello, %lu\n", (int64_t)head->data);
-    head = head->next;
-  }
-  printf("\n");
   remove_element_ll(&mine, (void *)2);
-  head = mine.head;
-  while (head != NULL) {
-    printf("hello, %lu\n", (int64_t)head->data);
-    head = head->next;
-  }
   delete_ll(&mine);
+  if (mine.head != NULL) {
+    printf("linked list head not null after trying to delete all elements\n");
+    failed = -1;
+  }
+  return failed;
 }
