e5e43abd65f626ffff036b7c8680446b0e8dd74b
twmb <travis.bischel@gmail.com>
2013-06-28 17:55:46 -0500

    return failed as 0 or -1 now, no extraneous printfs

--- c/sort/insertion_sort_test.c
+++ c/sort/insertion_sort_test.c
@@ -3,6 +3,7 @@
 #include "insertion_sort.h"
 
 int main(int argc, char **argv) {
+  int failed = 0;
   int us1[1] = {-1};
   int us2[5] = {4,3,2,1,2};
   insertion_sort(us1, 0, 0);
@@ -11,9 +12,9 @@ int main(int argc, char **argv) {
   for (int i = 0; i < 5; i++) {
     if (us2[i] < prev) {
       printf("us2 error: index %d, val %d > index %d, val %d\n", i, us2[i], i-1, prev);
-      return -1;
+      failed = -1;
     }
   }
-  printf("all good\n");
+  return failed;
 }
 
