3f959e651adeead2255c803415d7b769bbf001d0
twmb <travis.bischel@gmail.com>
2013-07-01 01:07:38 -0500

    add eclipse extraneous to gitignore, beginnings of matrix chain multiply

---
+++ java/project/src/dnyamic/matrixChainMultiply/MatrixInfo.java
@@ -0,0 +1,14 @@
+package dnyamic.matrixChainMultiply;
+
+public class MatrixInfo {
+	public int rows, cols;
+	
+	public MatrixInfo(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+	}
+	
+	public void chainMultiply(MatrixInfo[] matrices) {
+		
+	}
+}
