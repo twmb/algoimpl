23f01b8fe4fca58ea633f13b53bba571efd4a11e
twmb <travis.bischel@gmail.com>
2013-07-05 01:43:12 -0500

    actually reuse buffers properly for correct inversions

--- go/various/inversions.go
+++ go/various/inversions.go
@@ -23,13 +23,13 @@ func inversionsCombine(left, right, combined []int) int {
 }
 
 // performs a mergesort while counting inversions
-func inversionsCount(array, sorted []int) int {
+func inversionsCount(array, buffer []int) int {
 	if len(array) <= 1 {
 		return 0
 	}
-	cleft := inversionsCount(array[:len(array)/2], sorted[:len(array)/2])
-	cright := inversionsCount(array[len(array)/2:], sorted[len(array)/2:])
-	ccross := inversionsCombine(array[:len(array)/2], array[len(array)/2:], sorted)
+	cleft := inversionsCount(buffer[:len(array)/2], array[:len(array)/2])
+	cright := inversionsCount(buffer[len(array)/2:], array[len(array)/2:])
+	ccross := inversionsCombine(array[:len(array)/2], array[len(array)/2:], buffer)
 	return cleft + ccross + cright
 }
 
@@ -37,7 +37,8 @@ func inversionsCount(array, sorted []int) int {
 // An inversion is when a smaller number appears after a larger number.
 // For example, [1,3,5,2,4,6] has three inversions: [3,2], [5,2] and [5,4].
 func Inversions(array []int) int {
-	sorted := make([]int, len(array))
-	count := inversionsCount(array, sorted)
+	buffer := make([]int, len(array))
+	copy(buffer, array[:])
+	count := inversionsCount(array, buffer)
 	return count
 }
