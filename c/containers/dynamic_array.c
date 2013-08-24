2e1435ee82eaa7196b2a62d95f58a327c76eec4d
twmb <travis.bischel@gmail.com>
2013-08-24 13:02:25 -0500

    fixed add, added set, need to update to use mutexes for slicing

--- c/containers/dynamic_array.c
+++ c/containers/dynamic_array.c
@@ -1,4 +1,5 @@
 #include <stdlib.h> 
+//#include <pthread.h>
 
 #include "dynamic_array.h"
 
@@ -6,24 +7,42 @@ struct dynamic_array {
   void **elements;
   int len;
   int cap;
+
+//  int *slicecount;
+//  
+//  pthread_mutexattr_t attr;
+//  pthread_mutex_t mutex;
 };
 
 dynarr create_dynarr(void) {
   dynarr r = malloc(sizeof(dynarr));
   r->len = 0;
   r->cap = 0;
+  
+//  pthread_mutexattr_init(&r->attr);
+//  pthread_mutex_init(&r->mutex, &r->attr);
   return r;
 }
 
 dynarr make_dynarr(int len, int cap) {
   dynarr r = malloc(sizeof(dynarr));
+  if (len < 0) {
+    exit(-1);
+  }
+  if (cap < len) {
+    cap = len;
+  }
   r->len = len;
   r->cap = cap;
-  r->elements = malloc(cap * sizeof(void*));
+  if (cap > 0) {
+    r->elements = malloc(cap * sizeof(void*));
+  }
   return r;
 }
 
 void destroy_dynarr(dynarr array) {
+  // pthread_mutex_lock(&array->mutex);
+  // if 
   free(array->elements);
   free(array);
 }
@@ -60,8 +79,12 @@ dynarr dynarr_slice(dynarr array, int from, int to) {
   return new;
 }
 
-void *dynarr_at(dynarr array, int i) {
-  return array->elements[i];
+void *dynarr_at(dynarr array, int position) {
+  return array->elements[position];
+}
+
+void dynarr_set(dynarr array, int position, void *element) {
+  array->elements[position] = element;
 }
 
 int dynarr_len(dynarr array) {
--- c/containers/dynamic_array.h
+++ c/containers/dynamic_array.h
@@ -8,7 +8,8 @@ dynarr make_dynarr(int len, int cap);
 void destroy_dynarr(dynarr array);
 void *dynarr_append(dynarr array, void *element);
 dynarr dynarr_slice(dynarr array, int from, int to);
-void *dynarr_at(dynarr array, int i);
+void *dynarr_at(dynarr array, int position);
+void dynarr_set(dynarr array, int position, void *element);
 int dynarr_len(dynarr array);
 
 #endif
