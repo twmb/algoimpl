b0e84d35d61bd5c5a69b94a13ecc056f04959569
twmb <travis.bischel@gmail.com>
2013-08-26 12:40:59 -0500

    removed pointer from forward declaration, all functions use and return pointers now

--- c/containers/dynamic_array.h
+++ c/containers/dynamic_array.h
@@ -1,15 +1,15 @@
 #ifndef TWMB_DYNARRAY
 #define TWMB_DYNARRAY
 
-typedef struct dynamic_array *dynarr;
+typedef struct dynamic_array dynarr;
 
-dynarr create_dynarr(void);
-dynarr make_dynarr(int len, int cap);
-void destroy_dynarr(dynarr array);
-void *dynarr_append(dynarr array, void *element);
-dynarr dynarr_slice(dynarr array, int from, int to);
-void *dynarr_at(dynarr array, int position);
-void dynarr_set(dynarr array, int position, void *element);
-int dynarr_len(dynarr array);
+dynarr *create_dynarr(void);
+dynarr *make_dynarr(int len, int cap);
+void destroy_dynarr(dynarr *array);
+void *dynarr_append(dynarr *array, void *element);
+dynarr *dynarr_slice(dynarr *array, int from, int to);
+void *dynarr_at(dynarr *array, int position);
+void dynarr_set(dynarr *array, int position, void *element);
+int dynarr_len(dynarr *array);
 
 #endif
